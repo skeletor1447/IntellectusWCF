af417d1d9508c5fb42a732421739e7843bdc591e
Emilio Montiel <emilio.montiel@hotmail.com>
2019-04-08 03:45:43 -0500

    se implementó el método ConsultarPorCorreoYPassword

--- WCFIntellectus/WCFIntellectus/Entidades/Usuario.cs
+++ WCFIntellectus/WCFIntellectus/Entidades/Usuario.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace WCFIntellectus.Entidades
 {
+    [DataContract]
     public class Usuario
     {
+        [DataMember]
         public long ID { get; set; }
+        [DataMember]
         public String Correo { get; set; }
+        [DataMember]
         public String Nick { get; set; }
         
     }
--- WCFIntellectus/WCFIntellectus/Services/UsuarioServices.svc.cs
+++ WCFIntellectus/WCFIntellectus/Services/UsuarioServices.svc.cs
@@ -13,13 +13,38 @@ namespace WCFIntellectus.Services
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione UsuarioServices.svc o UsuarioServices.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class UsuarioServices : IUsuarioServices
     {
+        WCFIntellectus.Model.IntellectusdbEntities intellectusdbEntities = new Model.IntellectusdbEntities();
         public UnicaRespuesta<Usuario> ConsultarPorCorreoYPassword(string correo, string password)
         {
             UnicaRespuesta<Usuario> respuesta = new UnicaRespuesta<Usuario>();
+            respuesta.Errores = new Dictionary<string, string>();
+            if(String.IsNullOrEmpty(correo) || String.IsNullOrEmpty(correo.Trim()))
+            {
+                respuesta.Error = true;
+                respuesta.Errores.Add("Correo", "El campo correo está vacio");
+            }
+
+            if (String.IsNullOrEmpty(password) || String.IsNullOrEmpty(password.Trim()))
+            {
+                respuesta.Error = true;
+                respuesta.Errores.Add("Password", "El campo password está vacio");
+            }
 
 
+            try
+            {
+                WCFIntellectus.Model.tblusuario usuariotbl = intellectusdbEntities.tblusuario.Where(x => x.Correo == correo && x.Password == password).Single();
+                Usuario usuario = new Usuario() { ID = usuariotbl.IdUsuario, Correo = usuariotbl.Correo, Nick = usuariotbl.Nick};
 
-
+                respuesta.Error = false;
+                respuesta.Entidad = usuario;
+            }
+            catch
+            {
+                respuesta.Error = true;
+                respuesta.Errores.Add("CampoErroneo", "Correo o password incorrecto.");
+            }
+            
 
             return respuesta;
         }
